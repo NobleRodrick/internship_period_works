478aaa3fe8896474f37f898a31d7f80b53d5dcea
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2025-03-31 15:59:17 +0100

    Dealing with the Async-await in node

---
+++ Learning_node/8-async-await/index.js
@@ -0,0 +1,31 @@
+function delayFn(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function delayedGreet(name){
+    await delayFn(2000);
+    console.log(`Hello ${name}`);
+}
+
+delayedGreet("Alloh Nkefor Rodrick");
+
+async function dividion(num1, num2) {
+    try {
+        if(num2 === 0){
+            throw new Error("Cannot divide by zero");}
+
+            return num1 / num2;
+    } catch(error){
+        console.log("error", error);
+        return null
+    }
+}
+
+async function mainFn() {
+    const result = await dividion(10, 0);
+    const result2 = await dividion(10, 5);
+    console.log(result);
+    console.log(result2);
+}
+
+mainFn();
