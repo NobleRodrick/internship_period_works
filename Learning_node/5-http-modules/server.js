f0d94745db9e766c3f8f6413f8634b9c9c5fb848
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2025-03-31 13:53:51 +0100

    Creating endpoints in our node baackend

--- Learning_node/5-http-modules/server.js
+++ Learning_node/5-http-modules/server.js
@@ -2,8 +2,8 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     console.log(req, "req");
-
-
+    res.writeHead(200, {"Content-Type": "text/plain"})
+    res.end("hello node js from http modeule");
 });
 
 const port = 3000;
